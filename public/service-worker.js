9ffa1d6237433c1c4952789aeded7760c7f5f028
Matthew Adamson <matthewladamson@gmail.com>
2020-11-17 17:50:01 -0800

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
-const iconSizes = ["72", "96", "128", "144", "152", "192", "384", "512"];
+const iconSizes = ["192", "512"];
 const iconFiles = iconSizes.map(
-  (size) => `/assets/images/icons/icon-${size}x${size}.png`
+  (size) => `/icons/icon-${size}x${size}.png`
 );
 const staticFilesToPreCache = [
   "/",
